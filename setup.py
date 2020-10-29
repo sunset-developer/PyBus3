135851060e0e1413aec3b0a015b50df8e33c7176
sunset <aidanstewart@sunsetdeveloper.com>
2020-10-28 23:29:55 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PyBus',
+    version='0.1.0',
+    packages=['tests'],
+    url='https://github.com/sunset-developer/Async-Pybus',
+    license='MIT',
+    author='sunset-developer',
+    author_email='aidanstewart@sunsetdeveloper.com',
+    description='A simple, efficient, and async eventbus for python 3.6+ '
+)
